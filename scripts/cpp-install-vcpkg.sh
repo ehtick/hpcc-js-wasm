33d08e26736f71b58d109163bb9c885512c852f2
Gordon Smith <GordonJSmith@gmail.com>
2024-11-05 07:17:05 +0000

    feat:  Bump DuckDB version to 1.1.3
    Bump vcpkg to 2024.10.21
    Bump emsdk to v3.1.71
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- scripts/cpp-install-vcpkg.sh
+++ scripts/cpp-install-vcpkg.sh
@@ -2,7 +2,7 @@
 
 # List of current vertsion can be found in https://github.com/microsoft/vcpkg/releases  ---
 # UPDATE README.md
-VCPKG_BUILD_TOOLS_VERSION=2024.09.23
+VCPKG_BUILD_TOOLS_VERSION=2024.10.21
 
 if [ ! -d "./vcpkg" ] 
 then
